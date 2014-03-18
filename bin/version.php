d088bc4952c67c71ec96078115917b2d8a6f6cac
David M Penner <pennedav@gmail.com>
2014-03-17 20:26:39 -0400

    Some stuff to get us going.

---
+++ bin/version.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Pennedav\Composer\Sandbox\Sandbox as ComposerSandbox;
+
+echo "Version: " . ComposerSandbox::VERSION . "\n";
---
+++ src/Pennedav/Composer/Sandbox/Sandbox.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Pennedav\Composer\Sandbox;
+
+class Sandbox
+{
+    const VERSION = '0.1.0';
+}
